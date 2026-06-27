df9dbbf05257ead09bc33bd87d2381263caf9aad
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 00:10:57 -0300

    chore: shareCode HT1XFBUQG7Q -> 2RHH4VAOZ9G (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=HT1XFBUQG7Q&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=2RHH4VAOZ9G&afp1=bingo&home=1' });
     res.end();
   }
