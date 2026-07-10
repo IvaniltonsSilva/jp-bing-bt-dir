209d68e0de45b34da1bcafde280a1e2394a42fff
JCPrime01 <vanzokiller@gmail.com>
2026-07-10 09:24:06 -0300

    chore: shareCode UH9RCMPC38T -> L3NIDD479W5 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=UH9RCMPC38T&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=L3NIDD479W5&afp1=bingo&home=1' });
     res.end();
   }
