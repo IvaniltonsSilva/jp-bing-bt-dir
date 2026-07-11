dac978ec9ea22a99eec39af94b2736230860c61a
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 12:03:05 -0300

    chore: shareCode L3NIDD479W5 -> NCMAB46OLHG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=L3NIDD479W5&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=NCMAB46OLHG&afp1=bingo&home=1' });
     res.end();
   }
