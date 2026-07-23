84d01fd0e00e574dd2ade971c3424c07c2af33db
JCPrime01 <vanzokiller@gmail.com>
2026-07-23 15:38:03 -0300

    chore: shareCode NCMAB46OLHG -> 020OTRQGSZL (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=NCMAB46OLHG&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=020OTRQGSZL&afp1=bingo&home=1' });
     res.end();
   }
