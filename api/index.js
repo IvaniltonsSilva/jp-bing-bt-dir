7e9792b8647e1646b9ca8511ebb18b92403a7f0f
JCPrime01 <vanzokiller@gmail.com>
2026-08-04 11:16:35 -0300

    chore: shareCode M54BT78BN0T -> 8COYFUE7DUZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=M54BT78BN0T&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=8COYFUE7DUZ&afp1=bingo&home=1' });
     res.end();
   }
