529ef9710b0b5f1ab48b7aa745000a267afb2c4a
JCPrime01 <vanzokiller@gmail.com>
2026-08-07 14:08:02 -0300

    chore: shareCode D9GBM1DBRJU -> FTU97CLE7FJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=D9GBM1DBRJU&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=FTU97CLE7FJ&afp1=bingo&home=1' });
     res.end();
   }
