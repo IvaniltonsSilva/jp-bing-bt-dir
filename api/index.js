198762e357b6b357063d28a74476fd3ca1dbffd2
JCPrime01 <vanzokiller@gmail.com>
2026-08-12 11:40:02 -0300

    chore: shareCode MWH7FLAQLYI -> 4ZYYJUFCGUY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=MWH7FLAQLYI&afp1=bingo&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/5km2c4we?shareCode=4ZYYJUFCGUY&afp1=bingo&home=1' });
     res.end();
   }
